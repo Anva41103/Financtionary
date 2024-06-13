eef68ec616c7f235905a039f7e4ec33d51176784
Aag2004 <aatmika.gan2608@gmail.com>
2024-06-14 00:07:06 +0530

    about card layout

--- src/AboutCards.jsx
+++ src/AboutCards.jsx
@@ -4,7 +4,7 @@ export function Card() {
 
     return (
         <>
-            <div className="bg-pink-300 rounded shadow-md grid place-items-center p-4 m-5 mt-20 ">
+            <div className="bg-pink-300 rounded shadow-md grid place-items-center p-4 m-5 mt-20 w-128">
                 {/*Profile Picture */}
                 <img src="./src/assets/logo.jpeg" alt="Team Member" className="w-56 h-56 rounded-full mb-4 shadow-lg" />
                 <div className="grid place-items-center">
@@ -18,18 +18,23 @@ export function Card() {
                     {/*Contact Information */}
                     <ul className="flex justify-center mb-4">
                         <li className="mr-4">
-                            <a href="#" className="text-gray-600 hover:text-gray-900">
-                                <i className="fab fa-twitter"></i>
+                            <a href="#" className="text-gray-600 hover:text-gray-900 text-2xl">
+                                <i className="fab fa-twitter "></i>
                             </a>
                         </li>
                         <li className="mr-4">
-                            <a href="#" className="text-gray-600 hover:text-gray-900">
+                            <a href="#" className="text-gray-600 text-2xl hover:text-gray-900">
                                 <i className="fab fa-linkedin"></i>
                             </a>
                         </li>
                         <li className="mr-4">
-                            <a href="mailto:johndoe@example.com" className="text-gray-600 hover:text-gray-900">
+                            <a href="mailto:johndoe@example.com" className="text-gray-600 hover:text-gray-900 text-2xl">
                                 <i className="fas fa-envelope"></i>
+                            </a>
+                        </li>
+                        <li className="mr-4">
+                            <a href="mailto:johndoe@example.com" className="text-gray-600 hover:text-gray-900 text-2xl">
+                                <i className="fab fa-github"></i>
                             </a>
                         </li>
                     </ul>
