c76e0ea6474e76b4b9a471f75b336e619cd45636
Anva41103 <Praanu03@gmail.com>
2024-06-15 19:08:20 +0530

    new stuff

--- src/Bloghead.jsx
+++ src/Bloghead.jsx
@@ -1,10 +1,18 @@
 import './App.css'
 
-export function BHead () {
+export function BHead() {
 
     return (
         <>
-            <div className='h-96'></div>
+            <div className="bgpic">
+            <div class="card">
+                <p><span>Microfinance: Empowering the Underbanked</span></p>
+                <p><span>Mutual Funds: Diversified Investment for All</span></p>
+                <p><span>MSME Sector in India: The Backbone of the Economy</span></p>
+                <p><span>Loan Against Property: Unlocking the Value of Your Assets</span></p>
+                <p><span>Property Insurance: Safeguarding Your Valuable Assets</span></p>
+            </div>
+            </div>
         </>
     )
 }
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -8,11 +8,11 @@ export function Nav() {
                 <div className="w-full fixed bg-pastel nav-bar flex flex-row justify-between h-16 rounded-2xl ">
                     <div className="flex flex-row gap-10 mx-4 justify-between">
                         <img className="h-12 m-2 w-12 rounded-full" src="./src/assets/logo.jpeg" alt="" />
-                        <Link to="/Home"><button  className="m-2 px-4 h-11 rounded-full border-2 active:scale-95 border-slate-300 hover:bg-slate-100 active:bg-slate-200 text-sm sm:text-base">Home</button></Link>
-                        <Link to="/Blog"><button className="m-2 px-4 h-11 rounded-full border-2 active:scale-95 border-slate-300 hover:bg-slate-100 active:bg-slate-200 text-sm sm:text-base">Blog</button></Link>
-                        <button className="m-2 px-4 h-11 rounded-full border-2 active:scale-95 border-slate-300 hover:bg-slate-100 active:bg-slate-200 text-sm sm:text-base">Dictionary</button>
-                        <Link to="/About"><button className="m-2 px-4 h-11 rounded-full border-2 active:scale-95 border-slate-300 hover:bg-slate-100 active:bg-slate-200 text-sm sm:text-base">About Us</button></Link>
-                        <button className="m-2 px-4 h-11 rounded-full border-2 active:scale-95 border-slate-300 hover:bg-slate-100 active:bg-slate-200 text-sm sm:text-base">Login</button>
+                        <Link to="/Home"><button  className="m-2 h-11 rounded-full navbutton">Home</button></Link>
+                        <Link to="/Blog"><button className="m-2 px-4 h-11 rounded-full navbutton">Blog</button></Link>
+                        <button className="m-2 px-4 h-11 rounded-full navbutton">Dictionary</button>
+                        <Link to="/About"><button className="m-2 px-4 h-11 rounded-full navbutton">About Us</button></Link>
+                        <button className="m-2 px-4 h-11 rounded-full navbutton">Login</button>
                     </div>
                     <div className="flex flex-row gap-10 mx-4 justify-between">
                         {/* <button className="m-2 px-4 h-11 rounded-full border-2 active:scale-95 border-slate-300 hover:bg-slate-100 active:bg-slate-200 text-sm sm:text-base">Home</button>
