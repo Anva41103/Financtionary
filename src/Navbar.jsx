ed5f058eda4c7df64d13dd8dac7a632bd0978629
Anva41103 <Praanu03@gmail.com>
2024-06-21 00:50:22 +0530

    new stuff 5

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -8,14 +8,20 @@ export function Nav() {
                 <div className="w-full fixed bg-pastel nav-bar flex flex-row justify-between h-16 rounded-2xl ">
                     <div className="flex flex-row gap-10 mx-4 justify-between">
                         <img className="h-12 m-2 w-12 rounded-full" src="./src/assets/logo.jpeg" alt="" />
-                        <Link to="/Home"><button  className="m-2 h-11 rounded-full navbutton">Home</button></Link>
+                        <Link to="/Home"><button className="m-2 h-11 rounded-full navbutton">Home</button></Link>
                         <Link to="/Blog"><button className="m-2 px-4 h-11 rounded-full navbutton">Blog</button></Link>
                         <button className="m-2 px-4 h-11 rounded-full navbutton">Dictionary</button>
                         <Link to="/About"><button className="m-2 px-4 h-11 rounded-full navbutton">About Us</button></Link>
                         <Link to="/Login"><button className="m-2 px-4 h-11 rounded-full navbutton">Login</button></Link>
                     </div>
-                    <div className="flex flex-row gap-10 mx-4 justify-between">
-                        <button className="active:scale-95 m-2 px-4 py-1 h-11 rounded-md text-black font-semibold text-sm sm:text-base button_wotd">WOTD</button>
+                    <div className="switch-container">
+                        <button className="btn">
+                            <span className="btn-text-one">WOTD</span>
+                            <span className="btn-text-two">Liquidity Ratio </span>
+                        </button>
+                        <div className="switch-content">
+                            A financial metric used to determine a company's ability to pay off its short-term debts obligations. Common liquidity ratios include the current ratio and quick ratio, which compare a company's liquid assets to its short-term liabilities.
+                        </div>
                     </div>
                 </div>
             </div>
