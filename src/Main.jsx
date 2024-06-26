f6b41fb9cdadb92ebd4bd3f6744c325695facb1d
Aag2004 <aatmika.gan2608@gmail.com>
2024-06-26 13:24:54 +0530

    try4

--- src/Main.jsx
+++ src/Main.jsx
@@ -72,7 +72,8 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <> */}
-      <RouterProvider router={router}/>
+      {/* <RouterProvider router={router}/> */}
+      <App/>
     {/* </> */}
   </React.StrictMode>,
 )
